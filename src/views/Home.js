b9e4de3e8d763bb4e9c08032f8c92c7c2ad4b273
loick <hommet.loick@gmail.com>
2021-03-19 08:49:58 +0100

    frist commit

---
+++ src/views/Home.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+
+export default class Home extends Component {
+
+    constructor(prop){
+        super(prop);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        <div className="row">
+            
+        </div>
+    }
+}
